d3465342989829fdc86bed7cad48b7beaf0dbed6
badrobotenergy <badrobotenergy@gmail.com>
2023-12-13 20:09:02 -0800

    On branch master Initial Commit
    Creation of csv_parser.rb to parse, manaipulate and write out a .csv
    file that will have the proper formatting to be imported by the CHIRP
    next radio configuration application.
    
    Basic parsing capability currently.
    
    Next Steps: Refine parsing abilty to show the data in the correct form
    and strip out all entries not in Washington State.

---
+++ csv_parser.rb
@@ -0,0 +1,12 @@
+# import csv library
+require 'csv'
+
+# array to hold repeater row objects and initiate headers
+repeaters_info = []
+headers = nil
+
+# iterate through .csv file, headers tag makes each row a ruby object
+CSV.foreach('washington_repeaters.csv', headers: true, header_converters: :symbol) do |row|
+  headers ||= row.headers
+  repeaters_info << row
+end

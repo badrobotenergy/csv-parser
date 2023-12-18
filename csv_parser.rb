a717ef3531ea85653a2da66c95527ffa20478641
badrobotenergy <badrobotenergy@gmail.com>
2023-12-17 18:33:24 -0800

    Cleaned up all comment lines to reflect a clear objective as well as identify what different code blocks set out to accomplish. Addressed rubocop concerns over a comment line being too long, trailing whitespace at the eng of lines throughout the file and using single quotes instead of double quotes for non-interpolated strings.

--- csv_parser.rb
+++ csv_parser.rb
@@ -1,14 +1,15 @@
-# Goals:
-# iterate through the entire file
-# regex out "near Seattle" "near Sultan" "near Vancouver" "near Spokane" and write those values to a new an array or hash
-#  convert line by line back to strings and write out to .csv file
+# Objective:
+# iterate through the entire csv file
+# match strings 'near Seattle' 'near Sultan' 'near Vancouver' 'near Spokane'
+# write matched values to a new an array of hashes
+# write matching lines out to location relevant .csv files
 
 # frozen_string_literal: true
 
 require 'csv'
 
 # array to hold repeater row objects and initiate headers
-# repeaters for each specific 
+# repeaters for each specific location
 repeaters_info_seattle = []
 repeaters_info_sultan = []
 repeaters_info_vancouver = []
@@ -16,36 +17,34 @@ repeaters_info_spokane = []
 repeaters_info_gold_bar = []
 headers = nil
 
-# iterate through .csv file, headers tag makes each row a ruby object
+# iterate through .csv file, headers tag makes each row an addressable ruby object
 CSV.foreach('washington_repeaters.csv', headers: true, header_converters: :symbol) do |row|
   headers ||= row.headers
-  # repeaters_info << row
 
-  # Add rows to specific arrays depending on the city.
-  if row[16].include? "near Seattle"
-    repeaters_info_seattle << row 
-  elsif row[16].include? "near Sultan"
-    repeaters_info_sultan << row 
-  elsif row[16].include? "near Vancouver"
-    repeaters_info_vancouver << row 
-  elsif row[16].include? "near Spokane"
-    repeaters_info_spokane << row 
-  elsif row[16].include? "near Gold Bar"
-    repeaters_info_gold_bar << row 
+  # Add rows to specific arrays depending on the locale.
+  if row[16].include? 'near Seattle'
+    repeaters_info_seattle << row
+  elsif row[16].include? 'near Sultan'
+    repeaters_info_sultan << row
+  elsif row[16].include? 'near Vancouver'
+    repeaters_info_vancouver << row
+  elsif row[16].include? 'near Spokane'
+    repeaters_info_spokane << row
+  elsif row[16].include? 'near Gold Bar'
+    repeaters_info_gold_bar << row
   end
-
 end
 
-# Create the CSV files.
+# Create the csv files and write matched rows to them
 
-CSV.open("seattle_repeaters.csv", "wb") do |csv|
+CSV.open('seattle_repeaters.csv', 'wb') do |csv|
   csv << headers
   repeaters_info_seattle.each do |row|
     csv << row
   end
 end
 
-CSV.open("sultan_repeaters.csv", "wb") do |csv|
+CSV.open('sultan_repeaters.csv', 'wb') do |csv|
   csv << headers
   repeaters_info_sultan.each do |row|
     csv << row
@@ -55,14 +54,14 @@ CSV.open("sultan_repeaters.csv", "wb") do |csv|
   end
 end
 
-CSV.open("vancouver_repeaters.csv", "wb") do |csv|
+CSV.open('vancouver_repeaters.csv', 'wb') do |csv|
   csv << headers
   repeaters_info_vancouver.each do |row|
     csv << row
   end
 end
 
-CSV.open("spokane_repeaters.csv", "wb") do |csv|
+CSV.open('spokane_repeaters.csv', 'wb') do |csv|
   csv << headers
   repeaters_info_spokane.each do |row|
     csv << row
